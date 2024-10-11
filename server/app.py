0b2e0f700922880968938a8d215d06676fcb05b3
Ngeny Gilbert <ngenygkip@gmail.com>
2024-10-11 09:41:51 +0300

    crud

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 
 from flask import Flask, jsonify, request, make_response
@@ -6,7 +7,7 @@ from flask_restful import Api, Resource
 
 from models import db, Plant
 
-app = Flask(__name__)
+app = Flask(__name__)  # Corrected _name_ to __name__
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
@@ -42,14 +43,36 @@ api.add_resource(Plants, '/plants')
 
 
 class PlantByID(Resource):
-
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.get(id)
+        if plant:
+            return plant.to_dict(), 200
+        return {'message': 'Plant not found'}, 404
+
+    def patch(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return {'message': 'Plant not found'}, 404
+
+        data = request.get_json()
+        if "is_in_stock" in data:
+            plant.is_in_stock = data["is_in_stock"]
+
+        db.session.commit()
+        return plant.to_dict(), 200  
+
+    def delete(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return {'message': 'Plant not found'}, 404
+
+        db.session.delete(plant)
+        db.session.commit()
+        return '', 204  
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # Corrected _name_ to __name__ and _main_ to __main__
     app.run(port=5555, debug=True)
